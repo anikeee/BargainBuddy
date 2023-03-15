1cdcdd4bb8d745876df04e9993a5f4a6c05f599e
MD Muid Hasan Anik <muid@tripiq.eu>
2023-03-15 21:28:34 +0000

    added data in main-content

--- bargain-buddy/src/product/AmazonApi.js
+++ bargain-buddy/src/product/AmazonApi.js
@@ -1,32 +1,32 @@
 import axios from "axios";
+import productData from "./productData.js";
 
 export const getProductDetails = async (asin, country) => {
+    // Check if you want to use the productData from JSON file instead of making API calls
+    if (process.env.REACT_APP_USE_LOCAL_DATA === "true") {
+        console.log(productData);
+        return productData;
+    }
+
     const options = {
-        method: 'GET',
-        url: 'https://amazon23.p.rapidapi.com/product-details',
-        params: {asin: asin, country: country},
+        method: "GET",
+        url: "https://amazon23.p.rapidapi.com/product-details",
+        params: { asin: asin, country: country },
         headers: {
-            'X-RapidAPI-Key': '6e3bc1d031msh5140980f27995c8p1559e5jsn6a707fc97c99',
-            'X-RapidAPI-Host': 'amazon23.p.rapidapi.com'
-        }
+            "X-RapidAPI-Key": "6e3bc1d031msh5140980f27995c8p1559e5jsn6a707fc97c98",
+            "X-RapidAPI-Host": "amazon23.p.rapidapi.com",
+        },
     };
 
     try {
         const response = await axios(options);
         const { data } = response;
         console.log(data);
-        if (data) {
-            return {
-                title: data.title || '',
-                image: data.main_image || '',
-                price: data.price && data.price.current_price ? data.price.current_price : '',
-                description: data.feature_bullets ? data.feature_bullets.join(', ') : ''
-            };
-        }
 
-        return null;
+        // Return the full data object instead of a subset
+        return data;
     } catch (error) {
-        console.error('Error:', error.response ? error.response.data : error);
+        console.error("Error:", error.response ? error.response.data : error);
         return null;
     }
 };
