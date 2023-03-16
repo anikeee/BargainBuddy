47bb2257882461074dd3db0fc67650ae14c583f9
MD Muid Hasan Anik <muid@tripiq.eu>
2023-03-16 20:33:40 +0000

    search bar working now

--- bargain-buddy/src/components/SearchBar.js
+++ bargain-buddy/src/components/SearchBar.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InputBase, Paper } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSearch(inputValue);
+    };
+
     return (
         <Paper
             component="form"
+            onSubmit={handleSubmit}
             sx={{
                 display: 'flex',
                 alignItems: 'center',
@@ -14,6 +26,8 @@ const SearchBar = () => {
             }}
         >
             <InputBase
+                value={inputValue}
+                onChange={handleChange}
                 sx={{
                     marginLeft: 1,
                     flex: 1
