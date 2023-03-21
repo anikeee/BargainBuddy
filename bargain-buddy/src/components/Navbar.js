a1b2f4aa48cd4edd34a6b86941ff6a8737b114c3
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-21 11:30:21 +0000

    added burger icon to navbar

--- bargain-buddy/src/components/Navbar.js
+++ bargain-buddy/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import LoginButton from './LoginButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
@@ -7,6 +8,15 @@ const Navbar = () => {
     return (
         <AppBar position="static">
             <Toolbar>
+                <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                >
+                    <MenuIcon />
+                </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     BargainBuddy
                 </Typography>
