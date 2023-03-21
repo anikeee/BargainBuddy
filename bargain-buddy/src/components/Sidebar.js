530411e0fdc30329706962361be1b3b0420adfda
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-21 11:53:47 +0000

    Added sidebar to navbar. Now when user clicks on burger icon, sidebar will appear from left

--- bargain-buddy/src/components/Sidebar.js
+++ bargain-buddy/src/components/Sidebar.js
@@ -5,30 +5,13 @@ import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
+import ListItemButton from "@mui/material/ListItemButton"; 
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 
-export default function Sidebar() {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
+export default function Sidebar({state, setState, toggleDrawer}) {
 
   const list = (anchor) => (
     <Box
@@ -67,9 +50,8 @@ export default function Sidebar() {
 
   return (
     <div>
-      {["left", "right", "top", "bottom"].map((anchor) => (
+      {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
           <Drawer
             anchor={anchor}
             open={state[anchor]}
@@ -81,6 +63,4 @@ export default function Sidebar() {
       ))}
     </div>
   );
-}
-
-// export default Sidebar;
+};
